8212db56d17196887fe44dbaa93bac30762f0d0e
ryanferrer <ryferr8122@gmail.com>
2020-12-28 20:27:28 -0600

    added PM2 ecosystem for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    script: './server/server.js',
+  }],
+  deploy : {
+    production : {
+      key  : '/home/ryden/.ssh/Heavy-Storm.pem',
+      user : 'ubuntu',
+      host : '3.139.68.111',
+      ref  : 'origin/master',
+      repo : 'git@github.com:RemoteReq/Stats.git',
+      path : '/home/ubuntu/',
+      'post-deploy' : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  }
+};
