450ac984c29715dc44ae9ec0e990973fff1bcc24
ryanferrer <ryferr8122@gmail.com>
2021-02-05 17:56:56 -0600

    ecosystem udpate

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production : {
       key  : '/home/ryden/.ssh/Heavy-Storm.pem',
       user : 'ubuntu',
-      host : '3.139.68.111',
+      host : '3.15.184.188',
       ref  : 'origin/master',
       repo : 'git@github.com:RemoteReq/Stats.git',
       path : '/home/ubuntu/',
