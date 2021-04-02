816f2f26aee8c4254aca8f629e72d19edbdc6130
subhankarCN <subhankar.ray@capitalnumbers.com>
2021-04-02 11:50:05 +0530

    export csv

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { Card2 } from '../../components/Card/Card.js';
 import './List.css';
+import CsvDownload from 'react-json-to-csv'
 
 const List = ({ data, name, Card }) => {
+  const [emailList, setEmailList] = useState([]);
+  useEffect(() => { 
+    if(data){
+      if(name === 'Subscribers'){
+        setEmailList( data.map(element => { return {'email': element.emailId } }) );
+      }else{
+        setEmailList( data.map(element => { return { 'name': element.fullName, 'email': element.email } }) );
+      }
+      
+    }
+  }, [data]);
+
   return (
     <div className="list"> 
       <h1>{name}</h1>
-
+      <CsvDownload data={emailList} />
       {
         data ? 
 
