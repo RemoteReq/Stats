0695bf7bfb4584ce581097453d198a29b4b08d24
ryanferrer <ryferr8122@gmail.com>
2021-05-19 18:11:01 -0500

    new fields for coupon

--- src/components/Coupon/Coupon.js
+++ src/components/Coupon/Coupon.js
@@ -1,6 +1,27 @@
+import e from 'express';
 import React, {Component} from 'react';
 
 class Coupon extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentCode: '',
+      currentDiscount: '',
+      discountAmount: 0,
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
 
   render() {
     return (
@@ -16,10 +37,25 @@ class Coupon extends Component {
 
         <input
           placeholder="Code"
+          name="currentCode"
+          onChange={this.handleChange}
         />
 
         <input
+          type="checkbox"
+          name="flat"
+        />
+
+        <input
+          type="checkbox"
+          name="percentage"
+        />
+
+
+        <input
           placeholder="Discount"
+          name="currentDiscount"
+          onChange={this.handleChange}
         />
 
         <button>
