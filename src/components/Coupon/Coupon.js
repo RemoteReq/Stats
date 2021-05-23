fd4827a76ef062d553b057d18a8a522dfe7d0ace
ryanferrer <ryferr8122@gmail.com>
2021-05-23 18:30:48 -0500

    create a coupon feature

--- src/components/Coupon/Coupon.js
+++ src/components/Coupon/Coupon.js
@@ -1,29 +1,74 @@
-import e from 'express';
 import React, {Component} from 'react';
+import axios from 'axios';
+
+// const url = 'https://dev.remotereq.com';
+const url = 'http://localhost:3030';
 
 class Coupon extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      currentCode: '',
-      currentDiscount: '',
-      discountAmount: 0,
-
+      discountType: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.submitCoupon = this.submitCoupon.bind(this);
   }
 
-  handleChange(e) {
-    e.preventDefault();
+  componentDidMount() {
+    axios.get(`${url}/api/admin/latestCoupon`)
+    .then(response => {
+      console.log(response);
 
-    this.setState({
-      [e.target.name]: e.target.value,
+      const latestCoupon = response.data[0];
+
+      this.setState({
+        latestCode: latestCoupon.code,
+        latestAmount: latestCoupon.amount,
+        latestDiscountType: latestCoupon.discountType,
+      })
     })
   }
 
+  submitCoupon(e) {
+    e.preventDefault();
+    const { code, amount, discountType } = this.state || '';
+
+    const coupon = {
+      code,
+      amount,
+      discountType,
+    }
+
+    this.setState({
+      code: '',
+      amount: '',
+      discountType: '',
+    });
+
+    axios.post(`${url}/api/admin/coupon`, coupon)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  toggleDiscountType() {
+
+  }
+
   render() {
+    const { discountType } = this.state;
+    const { latestCode, latestAmount, latestDiscountType } = this.state || '';
+
     return (
       <div className="list">
 
@@ -31,36 +76,90 @@ class Coupon extends Component {
 
         <h2>Current Coupon</h2>
 
-        <h3>Code:</h3>
-
-        <h3>Discount:</h3>
-
-        <input
-          placeholder="Code"
-          name="currentCode"
-          onChange={this.handleChange}
+        <h3>Code: </h3>
+        <input 
+          readOnly
+          defaultValue={latestCode}
+          style={{backgroundColor: 'lightGray', border: 'none', outline: 'none'}}
         />
 
-        <input
-          type="checkbox"
-          name="flat"
+        <h3>Discount: {latestDiscountType}</h3>
+        <input 
+          readOnly
+          defaultValue={latestAmount}
+          style={{backgroundColor: 'lightGray', border: 'none', outline: 'none'}}
         />
 
-        <input
-          type="checkbox"
-          name="percentage"
-        />
+        <h3>Create new coupon</h3>
+          <input
+            placeholder="Code"
+            name="code"
+            onChange={this.handleChange}
+            />
 
+          <input
+            placeholder="Discount"
+            name="amount"
+            onChange={this.handleChange}
+          />
 
-        <input
-          placeholder="Discount"
-          name="currentDiscount"
-          onChange={this.handleChange}
-        />
+          <div>
+            <p>Discount Type</p>
 
-        <button>
-          Generate
-        </button>
+            <label>
+              <input
+                type="radio"
+                name="discountType"
+                value="flat"
+                checked={discountType === "flat"}
+                onChange={this.handleChange}
+                />
+              &nbsp; Flat
+            </label>          
+
+            <label>
+              <input
+                type="radio"
+                name="discountType"
+                value="percentage"
+                checked={discountType === "percentage"}
+                onChange={this.handleChange}
+                style={{
+                  marginLeft: '20px'
+                }}
+                />
+                &nbsp; Percentage
+              </label>
+          </div>
+
+          {/* <div>
+            <p>Payment Gateway</p>
+
+            <label>
+              <input 
+                type="checkbox"
+              />
+              &nbsp; First
+            </label>
+
+            <label>
+              <input 
+                type="checkbox"
+                style={{
+                  marginLeft: '20px'
+                }}
+              />
+              &nbsp; Second
+            </label>
+          </div> */}
+
+          <br/>
+
+          <button
+            onClick={this.submitCoupon}
+            >
+            Create
+          </button>
       </div>
     )
   }
