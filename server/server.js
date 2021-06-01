68fe9f21edd960b064852bdb9d9ffdc17ae1a9ce
ryanferrer <ryferr8122@gmail.com>
2021-06-01 09:00:39 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config('./.env');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 8080;
@@ -9,5 +10,13 @@ const auth = require('./auth.js');
 app.use(express.static('build'));
 
 app.use(auth);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.listen(PORT, () => {console.log(`Analytics online on port ${PORT}`)});
